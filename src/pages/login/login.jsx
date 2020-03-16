dbbd20bb52d218b44fa3cb8261e28d2dc3b3b122
KangTao <1421458857@qq.com>
2020-03-16 21:45:29 +0800

    更行

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import Logo from './images/logo.png'
 import './css/login.less'
-import Loginform from '../login_from/login_from';
 
 export default class Login extends Component {
+  onFinish = values => {
+    console.log('表单提交了');
+  };
 
+  //自定义校验
+  pwdValidator = (rule, value) => {
+    if (!value) return Promise.reject('密码不能为空!')
+    else if (value.length <= 4) return Promise.reject('密码必须大于4位!')
+    else if (value.length >= 12) return Promise.reject('密码必须小于12位!')
+    else if(!(/^\w+$/).test(value)) return Promise.reject('密码必须是字母、数字或下划线组成！')
+    return Promise.resolve()
+  }
   render() {
+    /*
+    用户名/密码的的合法性要求
+      1). 必须输入
+      2). 必须大于等于4位
+      3). 必须小于等于12位
+      4). 必须是字母、数字或下划线组成
+  */
+    const { Item } = Form
     return (
       <div id='login'>
         <header className="login-header">
@@ -13,8 +33,43 @@ export default class Login extends Component {
           <h1>商品管理系统</h1>
         </header>
         <div className="login-content">
-          <h2>用户登录</h2>
-          <Loginform/>
+          <h1>用户登录</h1>
+          <Form
+            name="normal_login"
+            className="login-form"
+            onFinish={this.onFinish}
+          >
+            <Item
+              name='username'
+              rules={[
+                { required: true, message: '用户名必须输入!' },
+                { min: 4, message: '用户名必须大于4位!' },
+                { max: 12, message: '用户名必须小于12位!' },
+                { pattern: /^\w+$/, message: '用户名必须是字母、数字或下划线组成!' }
+              ]}>
+              <Input
+                prefix={<UserOutlined />} placeholder="用户名" allowClear />
+            </Item>
+
+            <Item
+              name='password'
+              rules={[
+                { validator: this.pwdValidator }
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="密码"
+              />
+            </Item>
+
+            <Item>
+              <Button type="primary" htmlType="submit" className='login-form-button'>
+                登录
+               </Button>
+            </Item>
+          </Form>
         </div>
       </div>
     )
